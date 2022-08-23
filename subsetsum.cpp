2044814648f5d5303408a77e5a7694639379da52
BrahmaTeja <80442374+DBrahmaTeja@users.noreply.github.com>
2022-08-23 15:16:18 +0530

    Code Added .

--- subsetsum.cpp
+++ subsetsum.cpp
@@ -102,3 +102,20 @@ public:
         return dp[n][sum/2];
    }
 }
+vector<vector<bool>> dp(n + 1, vector<bool>(k + 1, false));
+
+for (int i = 0; i <= n; i++)
+{
+    for (int j = 0; j <= k; j++)
+    {
+        if (j == 0)
+            dp[i][j] = true;
+        else if (i == 0)
+            dp[i][j] = false;
+
+        else if (j < nums[i - 1])
+            dp[i][j] = dp[i - 1][j];
+        else
+            dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+    }
+}
